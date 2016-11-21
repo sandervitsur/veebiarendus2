3f40749a56ce9f15c49bbc543e4e50c2369d8870
pandaveeb.eu <vhost52181ssh@atria.elkdata.ee>
2016-11-21 12:29:59 +0000

    added template files

---
+++ 404.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * 404 Page for our theme
+ *
+ * @package WordPress
+ * @subpackage Integral
+ * @since Integral 1.0
+ */
+?>
+<?php get_header(); ?>
+<div class="spacer"></div>
+<div class="container">
+	<div class="row">
+
+		<div class="col-md-8">
+			<div class="content">
+				
+			
+                <h2><?php _e('Error 404 : Page not found!', 'integral'); ?></h2>
+                <p><?php _e('The page you\'re trying to locate is missing.', 'integral'); ?></p>
+			
+
+			</div><!--content-->
+		</div>
+
+		<?php get_sidebar(); ?>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
+
+
+
+
+
---
+++ woocommerce.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * The template for displaying woocommerce content.
+ *
+ * This is the template that displays all woocommerce content.
+ *
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="spacer"></div>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="col-md-8">
+			
+			<div class="content">			
+			
+                <?php woocommerce_content(); ?>
+
+			</div>
+
+		</div>
+
+		<?php get_sidebar(); ?>
+
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
